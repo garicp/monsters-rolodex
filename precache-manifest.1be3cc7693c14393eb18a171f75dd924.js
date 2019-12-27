ad35466dd42118438124a9bfad5ace021cd92deb
garicp <garicon@gmail.com>
2019-12-27 17:15:38 +0800

    Updates

---
+++ precache-manifest.1be3cc7693c14393eb18a171f75dd924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed16da520c6faf6b6dc9a903a38c7b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa2be8ecb866ec0272b3",
+    "url": "/monsters-rolodex/static/css/main.ae166dca.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa2be8ecb866ec0272b3",
+    "url": "/monsters-rolodex/static/js/main.ecec8e78.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
